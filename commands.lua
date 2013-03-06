2f778dc5199e1971da1062583652ab930e30a1f9
STRWarrior <niels.breuker@hotmail.nl>
2013-03-06 10:07:10 -0800

    Better config

--- commands.lua
+++ commands.lua
@@ -3,7 +3,7 @@
 function HandleChangePasswordCommand( Split, Player)
 	if Auth[Player:GetName()] == true then
 		if (Split[2] == nil) then
-			Player:SendMessage(cChatColor.LightGreen .. "Usage: /changepass (oldpassword) (newpassword)")
+			Player:SendMessage(cChatColor.LightGreen .. ChangePassword)
 		else
 			PlayerPass = (io.open(PluginDir .. "Players/" .. Player:GetName(), "r"))
 			Password = (io.lines(PluginDir .. "Players/" .. Player:GetName()))
@@ -14,7 +14,7 @@ function HandleChangePasswordCommand( Split, Player)
 				AuthDir[Player:GetName()]:close()
 				Player:SendMessage(cChatColor.LightGreen .. "Password changed to " .. Split[3])
 			else
-				Player:SendMessage(cChatColor.Rose .. "your old password is wrong. Usage: /changepass (oldpassword) (newpassword)")
+				Player:SendMessage(cChatColor.Rose .. ChangePasswordWrong)
 			end
 		end
 	end
@@ -28,7 +28,7 @@ function HandleLogoutCommand( Split, Player )
 	Z[Player:GetName()] = Player:GetPosZ()
 	Count[Player:GetName()] = Tries
 	Auth[Player:GetName()] = false
-	Player:SendMessage(cChatColor.LightGreen .. "You are now logged out")
+	Player:SendMessage(cChatColor.LightGreen .. Logout)
 	Count[Player:GetName()] = Tries
 	return true
 end
@@ -36,14 +36,14 @@ end
 function HandleLoginCommand( Split, Player)
 	if Auth[Player:GetName()] == false then
 		if (Split[2] == nil) then
-		Player:SendMessage(cChatColor.LightGreen .. "Usage: /login (password)")
+		Player:SendMessage(cChatColor.LightGreen .. Login)
 		else
 			local PlayerPass = io.open(PluginDir .. "Players/" .. Player:GetName(), "r")
 			if PlayerPass then
 				local line = PlayerPass:read("*all")
 				if line == md5(Split[2]) then
 					Auth[Player:GetName()] = true
-					Player:SendMessage(cChatColor.LightGreen .. "You are logged in")
+					Player:SendMessage(cChatColor.LightGreen .. LoggedIn)
 					Player:LoadPermissionsFromDisk()
 					Player:TeleportTo( X[Player:GetName()], Y[Player:GetName()], Z[Player:GetName()] )
 				else
@@ -51,15 +51,22 @@ function HandleLoginCommand( Split, Player)
 					Player:SendMessage(cChatColor.Rose .. "Wrong Password, You have " .. Count[Player:GetName()] .. " tries left")
 				if Count[Player:GetName()] == 0 then
 					Player:TeleportTo( X[Player:GetName()], Y[Player:GetName()], Z[Player:GetName()] )
-					local ClientHandle = Player:GetClientHandle()
-					ClientHandle:Kick( "You used a wrong password too many times" )
+					if Ban == true then
+						PluginManager = cRoot:Get():GetPluginManager()
+						local CorePlugin = PluginManager:GetPlugin("Core")
+
+						CorePlugin:Call("HandleBanCommand", Player:GetName(), Player:GetName())
+					else
+						local ClientHandle = Player:GetClientHandle()
+						ClientHandle:Kick( "You used a wrong password too many times" )
+					end
 				end
 			end
 			PlayerPass:close()
 		end
 	end             
 	else
-		Player:SendMessage(cChatColor.LightGreen .. "You are already logged in")
+		Player:SendMessage(cChatColor.LightGreen .. AlreadyLoggedIn)
 	end
 	return true
 end
@@ -67,18 +74,18 @@ end
 
 function HandleRegisterCommand( Split, Player )
 	if (Split[2] == nil) then
-		Player:SendMessage(cChatColor.Rose .. "Usage: /register (password)")
+		Player:SendMessage(cChatColor.Rose .. Register)
 	else
 		AuthDir[Player:GetName()] = io.open(PluginDir .. "Players/" .. Player:GetName(), "r" )
 		PDIP = io.open(PluginDir .. "IP/" .. Player:GetName(), "w")
 		if AuthDir[Player:GetName()] then
-			Player:SendMessage(cChatColor.Rose .. "you already have registered")
+			Player:SendMessage(cChatColor.Rose .. AlreadyRegistered)
 		else
 			Auth[Player:GetName()] = true
 			AuthDir[Player:GetName()] = io.open(PluginDir .. "Players/" .. Player:GetName(), "w" )
 			AuthDir[Player:GetName()]:write(md5(Split[2]))
 			AuthDir[Player:GetName()]:close()
-			Player:SendMessage(cChatColor.Green .. "You have registered")
+			Player:SendMessage(cChatColor.Green .. Registered)
 		end
 	end
 	return true
@@ -87,7 +94,7 @@ end
 
 function HandleRemoveAccountCommand( Split, Player )
 	if (Split[2] == nil) then
-		Player:SendMessage(cChatColor.LightGreen .. "Usage: /removeacc (player)")
+		Player:SendMessage(cChatColor.LightGreen .. RemoveAcc)
 	else
 		AuthDir[Split[2]] = io.open(PluginDir .. "Players/" .. Split[2], "r" )
 		if AuthDir[Split[2]] then
