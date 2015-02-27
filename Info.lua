5b4e41f12dbf58928be3711f4319846fc6028ccc
STRWarrior <niels.breuker@hotmail.nl>
2015-02-27 17:39:52 +0100

    Info.lua: Added info about the plugin and how to configurate it.

--- Info.lua
+++ Info.lua
@@ -9,7 +9,11 @@ g_PluginInfo =
 	Version = "2",
 	Date = "2015-02-21",
 	SourceLocation = "https://github.com/NiLSPACE/Login",
-	Description = [[]],
+	Description = [[
+Login is a security plugin for MCServer. 
+It requires a player to log in before they can do any action on the server. 
+This way people can't login on the account of an admin or moderator.
+This would be particuarly useful for servers that don't use the traditional Minecraft authentication.]],
 	
 	Commands =
 	{
@@ -54,6 +58,22 @@ g_PluginInfo =
 		{
 			Description = "Allows a player to change his password.",
 			RecommendedGroups = "Default",
+		},
+	},
+	
+	AdditionalInfo =
+	{
+		{
+			Title = "Configuration",
+			Contents = [[
+If a configuration file doesn't exist yet Login will create one with the default settings.
+Currently there are 2 or 3 things you can configurate depending on the password storage you choose.
+	Storage: This can be "sqlite" (default) or "file". If choosen for file then the plugin will save all the passwords in a file, and if choosen for sqlite the plugin will save everything in an sqlite database.
+	LoginMessageTime: This is the time in ticks (50 msec) between an "You have to login" message.
+	
+	If the storage is set to file then you can also change the compression of the password file.
+	CompressionLevel: A number where 0 is no compression and 9 is maximum compression.
+			]],
 		},
 	},
 }
