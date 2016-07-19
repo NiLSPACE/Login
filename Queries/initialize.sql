882d767e8bf24afd40a72ab7b2de1e3ee77cd4cd
NiLSPACE <niels.breuker@hotmail.nl>
2016-07-19 14:14:36 +0200

    Finally updated initialize query

--- Queries/initialize.sql
+++ Queries/initialize.sql
@@ -2,20 +2,3 @@ CREATE TABLE IF NOT EXISTS "players" (
 	`uuid`	TEXT PRIMARY KEY,
 	`password` TEXT
 );
-
-CREATE TABLE IF NOT EXISTS "temptable" (
-	`uuid` TEXT,
-	`password` TEXT
-);
-
-INSERT INTO "temptable" SELECT DISTINCT(`uuid`), `password` FROM "players" GROUP BY `uuid`;
-
-DROP TABLE "players";
-CREATE TABLE IF NOT EXISTS "players" (
-	`uuid`	TEXT PRIMARY KEY,
-	`password` TEXT
-);
-
-INSERT INTO "players" SELECT * FROM "temptable";
-DROP TABLE "temptable"
-
